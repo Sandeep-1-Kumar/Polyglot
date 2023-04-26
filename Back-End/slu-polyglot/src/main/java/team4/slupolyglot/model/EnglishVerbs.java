d2c3eb00bdd792bd13d3e313afc0a9ddff504b94
fulvius31 <mail@alessandrosangiorgi.net>
2023-04-25 21:45:20 -0500

    Handle past and perfect english irregularities(WIP)
    Ref #68

--- Back-End/slu-polyglot/src/main/java/team4/slupolyglot/model/EnglishVerbs.java
+++ Back-End/slu-polyglot/src/main/java/team4/slupolyglot/model/EnglishVerbs.java
@@ -24,7 +24,7 @@ public class EnglishVerbs {
 
     private String tense;
     private boolean isNegative = false;
-    public final static String[] ENGLISH_PRONOUNS = {"I","You","(s)he","We","You","They"};
+    public final static String[] ENGLISH_PRONOUNS = {"I ","you ","(s)he ","we ","you ","they "};
     HashMap<String, String> pronouns = new HashMap<>() {{
         put(GENERAL_PRONOUNS[0], ENGLISH_PRONOUNS[0]);
         put(GENERAL_PRONOUNS[1], ENGLISH_PRONOUNS[1]);
@@ -69,15 +69,66 @@ public class EnglishVerbs {
         }
     }
 
-    private String present() {
-        String negation = isNegative ? " do not " : "";
 
+    private String treatTheBePresent(){
+        String negative = isNegative ? " not" : "";
         if (pronouns.get(pronoun).equals(ENGLISH_PRONOUNS[2]))
-            return (ENGLISH_PRONOUNS[2] + negation + " " + this.verb+"s");
+            return "is" + negative;
+        else if (pronouns.get(pronoun).equals(ENGLISH_PRONOUNS[0]))
+            return "am" + negative;
+
+        return "are" + negative;
+    }
+    private String getPresent() {
+        String[] composedVerb = this.verb.split(" ");
+        if(composedVerb[0].equals("be")){
+            composedVerb[0] = treatTheBePresent();
+        } else if (pronouns.get(pronoun).equals(ENGLISH_PRONOUNS[2])) {
+            char lastChar = composedVerb[0].charAt(composedVerb[0].length() - 1);
+            if (lastChar == 'y') {
+                if (!isVowel(composedVerb[0].charAt(composedVerb[0].length() - 2))) {
+                    if(composedVerb.length == 1)
+                        return composedVerb[0].substring(0, composedVerb[0].length() - 1) + "ies";
+                    else
+                        return composedVerb[0].substring(0, composedVerb[0].length() - 1) + "ies"
+                                + " " +composedVerb[1];
+                }
+            } else if (lastChar == 'h') {
+                if (!isVowel(composedVerb[0].charAt(composedVerb[0].length() - 2))) {
+                    if(composedVerb.length == 1)
+                        return composedVerb[0].substring(0, composedVerb[0].length()) + "es";
+                    else
+                        return composedVerb[0].substring(0, composedVerb[0].length()) + "es"
+                                + " " +composedVerb[1];
+                }
+            }
+            else {
+                if(composedVerb.length == 1)
+                    return composedVerb[0] + "s";
+                else
+                    return composedVerb[0] + "s" + " " +composedVerb[1];
+            }
+        }
+        if(composedVerb.length == 1)
+            return composedVerb[0];
+        else
+            return composedVerb[0] + " " +composedVerb[1];
+    }
+    // todo born without s
+    private String present() {
+        String[] composedVerb = this.verb.split(" ");
+        boolean isBe = composedVerb[0].equals("be");
+        String negation = null;
+        if(!isBe)
+            negation = isNegative ? "do not " : "";
 
         for (String englishPronoun : ENGLISH_PRONOUNS) {
-            if (pronouns.get(pronoun).equals(englishPronoun))
-                return (englishPronoun + negation + " " + this.verb);
+            if (pronouns.get(pronoun).equals(englishPronoun)) {
+                if(!isBe)
+                    return (englishPronoun + negation + getPresent());
+                return (englishPronoun  + getPresent());
+            }
+
         }
         throw new IllegalArgumentException("Invalid pronoun");
     }
@@ -123,9 +174,9 @@ public class EnglishVerbs {
     }
     private String past() throws IOException {
 
-        if (!findIrregularities(this.verb) || (!isNegative && !this.verb.contains("be"))){
-            String negation = isNegative ? " did not " : " ";
-            String past = isNegative ? "" : getRegularPast();
+        if (!findIrregularities(this.verb) || (isNegative && !this.verb.contains("be"))){
+            String negation = isNegative ? "did not " : "";
+            String past = isNegative ? this.verb : getRegularPast();
             for (String englishPronoun : ENGLISH_PRONOUNS) {
                 if (pronouns.get(pronoun).equals(englishPronoun))
                     return (englishPronoun  + negation + past);
@@ -137,7 +188,7 @@ public class EnglishVerbs {
                 String past = mapIrregularity(this.verb, IRREGULAR_PAST);
                 for (String englishPronoun : ENGLISH_PRONOUNS) {
                     if (pronouns.get(pronoun).equals(englishPronoun))
-                        return (englishPronoun + " " + past);
+                        return (englishPronoun + past);
                 }
             }
         }
@@ -145,22 +196,22 @@ public class EnglishVerbs {
         throw new IllegalArgumentException("Invalid pronoun");
     }
     private String future(){
-        String negation = isNegative ? " not " : "";
+        String negation = isNegative ? "not " : "";
 
         for (String englishPronoun : ENGLISH_PRONOUNS) {
             if (pronouns.get(pronoun).equals(englishPronoun))
-                return (englishPronoun  + " will "+ negation + this.verb);
+                return (englishPronoun  + "will "+ negation + this.verb);
         }
         throw new IllegalArgumentException("Invalid pronoun");
     }
     private String imperative() { //todo complete
-        String negation = isNegative ? " do not " : "";
+        String negation = isNegative ? "do not " : "";
         if(pronouns.get(pronoun).equals(ENGLISH_PRONOUNS[2]))
             throw new IllegalArgumentException("Invalid pronoun, cannot be 3s");
 
         for (String englishPronoun : ENGLISH_PRONOUNS) {
             if (pronouns.get(pronoun).equals(englishPronoun))
-                return (englishPronoun + negation + " " + this.verb);
+                return (negation + this.verb);
         }
         throw new IllegalArgumentException("Invalid pronoun");
     }
@@ -168,8 +219,8 @@ public class EnglishVerbs {
     //todo able
     private String perfect() throws IOException {
         boolean is3s = pronouns.get(pronoun).equals(ENGLISH_PRONOUNS[2]);
-        String haveHas = is3s ? " has " : " have ";
-        String negation = isNegative ? " not " : "";
+        String haveHas = is3s ? "has" : "have";
+        String negation = isNegative ? " not" : "";
 
         if(!findIrregularities(this.verb)) {
             String pastEd = getRegularPast();
@@ -227,8 +278,11 @@ public class EnglishVerbs {
         String line;
 
         while ((line = reader.readLine()) != null) {
-            if(line.split("\t")[0].equals(verb))
-                return line.split("\t")[index];
+            if(line.split("\t")[0].equals(verb)){
+                    //System.out.println(line.split("\t")[index]);
+                    System.out.println(this.isNegative+"+"+this.pronoun+"+"+this.tense+"+"+this.verb);
+                    return line.split("\t")[index];
+                }
         }
 
         throw new IllegalArgumentException("This verb has no irregular form");
